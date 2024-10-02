8df6cac82c7872078465b87828d2df95232375ce
enayetaitech <mdrehman@365aitech.com>
2024-10-02 19:51:12 +0600

    corrected again

--- src/app/(auth)/verify-account/page.jsx
+++ src/app/(auth)/verify-account/page.jsx
@@ -1,63 +1,44 @@
-'use client';
 import VerificationErrorUI from "@/components/authComponent/VerificationErrorUI";
 import VerifyAccountUI from "@/components/authComponent/VerifyAccountUI";
 import Logo from "@/components/shared/Logo";
 import axios from "axios";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
 
-const VerifyAccount = () => {
-  const searchParams = useSearchParams();
-  const id = searchParams.get('id');
-  const [verificationStatus, setVerificationStatus] = useState(null);
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true); // Ensure the code is running on the client
-
-    if (id) {
-      const verifyEmail = async () => {
-        try {
-          console.log('sending verification request');
-          const response = await axios.get(`http://localhost:8008/api/verify?id=${id}`);
-          console.log('response .data', response.data);
-          
-          if (response.status === 200) {
-            setVerificationStatus('success');
-          }
-        } catch (error) {
-          setVerificationStatus('error');
-          console.error("Verification error:", error);
-        }
-      };
-      
-      verifyEmail();
-    }
-  }, [id]);
-
-  if (!isClient) {
-    // Return a placeholder until the component is fully mounted on the client
-    return <p className="min-h-screen text-center">Loading...</p>;
-  }
-
+const VerifyAccount = ({ verificationStatus }) => {
   return (
     <div>
       <div className="flex justify-center items-center pt-5 lg:hidden">
         <Logo />
       </div>
       <div className="pt-5 pl-10 lg:block hidden">
-        {' '}
         <Logo />
       </div>
       {verificationStatus === 'success' ? (
         <VerifyAccountUI />
       ) : verificationStatus === 'error' ? (
-        <VerificationErrorUI/>
+        <VerificationErrorUI />
       ) : (
-        <p className="min-h-screen text-center">Verifying your account...</p> // Loading message while waiting for the request
+        <p className="min-h-screen text-center">Verifying your account...</p>
       )}
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  let verificationStatus = null;
+
+  try {
+    const response = await axios.get(`http://localhost:8008/api/verify?id=${id}`);
+    if (response.status === 200) {
+      verificationStatus = 'success';
+    }
+  } catch (error) {
+    verificationStatus = 'error';
+  }
+
+  return {
+    props: { verificationStatus },
+  };
 }
 
-export default VerifyAccount
+export default VerifyAccount;
