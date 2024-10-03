5c82075fa7469fce71eeb824abfda33c96b1d03e
enayetaitech <mdrehman@365aitech.com>
2024-10-03 15:32:13 +0600

    account activation wrapped by suspense

--- src/app/(auth)/account-activation/page.jsx
+++ src/app/(auth)/account-activation/page.jsx
@@ -1,20 +1,30 @@
-'use client'
-import AccountActivationUI from '@/components/authComponent/AccountActivationUI'
-import Logo from '@/components/shared/Logo'
-import { useSearchParams } from 'next/navigation'
+'use client';
+import AccountActivationUI from '@/components/authComponent/AccountActivationUI';
+import Logo from '@/components/shared/Logo';
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
+const AccountActivationContent = () => {
+  const searchParams = useSearchParams();
+  const email = searchParams.get('email');
+  console.log('email', email);
+  return <AccountActivationUI email={email} />;
+};
 
 const AccountActivation = () => {
-  const searchParams = useSearchParams()
-  const email = searchParams.get('email')
-  console.log('emaiil', email)
   return (
     <div>
-      <div className='flex justify-center items-center pt-5 lg:hidden'>  <Logo/></div>
-      <div className='pt-5 pl-10 lg:block hidden'>  <Logo/></div>
-        <AccountActivationUI email={email}/>
+      <div className='flex justify-center items-center pt-5 lg:hidden'>
+        <Logo />
+      </div>
+      <div className='pt-5 pl-10 lg:block hidden'>
+        <Logo />
+      </div>
+      <Suspense fallback={<p className="min-h-screen text-center">Loading...</p>}>
+        <AccountActivationContent />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default AccountActivation
+export default AccountActivation;
